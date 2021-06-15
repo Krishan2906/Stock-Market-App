cab79196fa2714371b977c0d13e31c5317872f49
KRISHAN AGARWAL <58983106+Krishan2906@users.noreply.github.com>
2021-06-15 12:38:04 +0530

    User Api uploaded

---
+++ User.Api/Entities/LoggedInUsers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.Api.Entities
+{
+    public class LoggedInUsers
+    {
+        [Key]
+        public string UserID { get; set; }
+        public DateTime Time { get; set; }
+
+    }
+}
